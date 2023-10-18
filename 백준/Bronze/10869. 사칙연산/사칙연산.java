3bfa34184ee06c187424ec1368bc2bebaa7cf78c
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2023-10-18 11:29:52 +0900

    [Bronze V] Title: 사칙연산, Time: 108 ms, Memory: 12864 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int a = input.nextInt();
+		int b = input.nextInt();
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+
+	}
+
+}
