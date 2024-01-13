2cb8b59bdb7d210697b0fd9a70ec9845b6bf585f
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-01-13 21:06:26 +0900

    [Bronze V] Title: 꼬마 정민, Time: 112 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long a = input.nextLong();
+        long b = input.nextLong();
+        long c = input.nextLong();
+
+        System.out.print(a + b + c);
+    }
+}
