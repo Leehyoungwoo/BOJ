ca84171c6b398a7461e1d560518f4ce10b1f4079
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-03-09 02:42:01 +0900

    [level 3] Title: n + 1 카드게임, Time: 2.36 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.java
@@ -0,0 +1,96 @@
+import java.util.*;
+
+class Solution {
+
+    int n;
+
+    public int solution(int coin, int[] cards) {
+        int round = 1;
+        n = cards.length;
+        int curIdx = n / 3;
+        List<Integer> possession = new ArrayList<>();
+        for (int i = 0; i < curIdx; i++) {
+            possession.add(cards[i]);
+        }
+        List<Integer> candidates = new ArrayList<>();
+
+        L:
+        while (true) {
+            /* 반복문 종료 조건 */
+            if (round > n / 3 + 1) { // 가능한 최대 라운드 넘어가면 종료
+                break;
+            }
+            if (curIdx >= n) { // 카드 뭉치가 모두 소진되면 종료
+                break;
+            }
+
+            /*  우선 후보군에 두 장 넣어둠 */
+            candidates.add(cards[curIdx++]);
+            candidates.add(cards[curIdx++]);
+
+            /* 소유 중인 카드에서 n+1 가능하면 coin 안 쓰고 다음 라운드 진출 */
+            if (findNPlusOne(possession)) {
+                round++;
+                continue;
+            }
+
+            /* 소유 카드 중 1개 + 후보군 1개 로 n+1 가능하면 코인 1개 소진 */
+            if (coin > 0) {
+                for (int i = 0; i < possession.size(); i++) {
+                    int target = n + 1 - possession.get(i);
+                    if (candidates.contains(target)) {
+                        possession.remove(possession.get(i));
+                        candidates.remove((Integer) target);
+                        coin--;
+                        round++;
+                        continue L;
+                    }
+                }
+            }
+
+            /* 후보군에서만 2장으로 n+1 만들 수 있는 경우에는 코인 2개 소진 */
+            if (coin > 1) {
+                if (findNPlusOne(candidates)) {
+                    coin -= 2;
+                    round++;
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        return round;
+    }
+
+    boolean findNPlusOne(List<Integer> list) {
+        if (list.isEmpty()) {
+            return false;
+        }
+        Collections.sort(list);
+
+        /* 이분탐색으로 타겟 찾기 */
+        for (int i = 0; i < list.size() - 1; i++) {
+            int target = n + 1 - list.get(i);
+
+            int l = 0;
+            int r = list.size() - 1;
+
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                if (list.get(mid) < target) {
+                    l = mid + 1;
+                } else if (list.get(mid) > target) {
+                    r = mid - 1;
+                } else {
+                    list.remove(mid);
+                    list.remove(i);
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
