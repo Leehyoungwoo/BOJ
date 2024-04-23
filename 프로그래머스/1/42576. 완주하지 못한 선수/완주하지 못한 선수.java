d389fe82988d5989608a4245c77b3588a17d854a
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-04-23 09:21:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 99.91 ms, Memory: 95.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        }
+        
+        for(int i = 0; i < completion.length; i++) {
+            String man = completion[i];
+            map.put(completion[i], map.getOrDefault(completion[i], 0) - 1);
+            if(map.get(man) == 0) {
+                map.remove(man);
+            }  
+        }
+        Set<String> set = map.keySet();
+        for(String name : set) {
+            answer = name;
+        }
+
+        return answer;
+    }
+}
