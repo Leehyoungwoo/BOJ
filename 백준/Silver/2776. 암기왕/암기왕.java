021f4ebc7681b0f9b8360aa79818bf2a25e68204
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-06-12 18:23:41 +0900

    [Silver IV] Title: 암기왕, Time: 1736 ms, Memory: 393368 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,34 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
+    private static int t;
+    private static int n;
+    private static int m;
+    private static Set<Integer> note1;
+    private static int[] note2;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
-
-        for(int a=0; a<t; a++){
-            Set<Integer> set = new HashSet<>();
-            StringBuilder sb = new StringBuilder();
-            int n = Integer.parseInt(br.readLine());
-
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for(int i=0; i<n; i++)
-                set.add(Integer.parseInt(st.nextToken()));
-
-            int m = Integer.parseInt(br.readLine());
-
-            st = new StringTokenizer(br.readLine());
-            for(int i=0; i<m; i++)
-                sb.append(set.contains(Integer.parseInt(st.nextToken()))? 1 : 0).append("\n");
-
-            System.out.print(sb);
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        t = Integer.parseInt(input.readLine());
+        StringBuilder finalAnswer = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            finalAnswer.append(init(input));
         }
+        System.out.print(finalAnswer);
+    }
+
+    private static StringBuilder init(BufferedReader input) throws IOException {
+        n = Integer.parseInt(input.readLine());
+        note1 = new HashSet<>();
+        StringTokenizer tokenizer = new StringTokenizer(input.readLine());
+        for (int i = 0; i < n; i++) {
+            note1.add(Integer.parseInt(tokenizer.nextToken()));
+        }
+        m = Integer.parseInt(input.readLine());
+        note2 = new int[m];
+        tokenizer = new StringTokenizer(input.readLine());
+        for (int i = 0; i < m; i++) {
+            note2[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            if (note1.contains(note2[i])) {
+                answer.append(1).append("\n");
+                continue;
+            }
+            answer.append(0).append("\n");
+        }
+        return answer;
     }
 }
