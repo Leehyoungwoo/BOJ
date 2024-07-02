7a09d4b9b84d2f6e842888edf741a41d2d5bc36b
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-07-02 17:29:16 +0900

    [Gold I] Title: K번째 수, Time: 120 ms, Memory: 11592 KB -BaekjoonHub

---
+++ 백준/Gold/1300. K번째 수/K번째 수.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(input.readLine());
+        long k = Long.parseLong(input.readLine());
+        long left = 1;
+        long right = n * (long)n;
+        long answer = 0;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            if (countLessOrEqual(mid, n) >= k) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    private static long countLessOrEqual(long mid, int n) {
+        long cnt = 0; 
+        for (int i = 1; i <= n; i++) {
+            cnt += Math.min(mid / i, n);
+        }
+        return cnt;
+    }
+}
