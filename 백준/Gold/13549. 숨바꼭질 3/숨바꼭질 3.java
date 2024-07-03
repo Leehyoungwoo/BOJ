b9840fc2033901aec56f94806734facbef74e002
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-07-03 23:18:15 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 148 ms, Memory: 19320 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    private static int[] moves = {-1, 1, 2};
+    private static int n;
+    private static int k;
+    private static int[] distance;
+    private final static int MAX = 100_001;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        findAnswer();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = input.readLine().split(" ");
+        n = Integer.parseInt(s[0]);
+        k = Integer.parseInt(s[1]);
+        distance = new int[MAX];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+    }
+
+    private static void findAnswer() {
+        Queue<Integer> que = new LinkedList<>();
+        distance[n] = 0;
+        que.add(n);
+        while (!que.isEmpty()) {
+            int cur = que.poll();
+            for (Integer m : moves) {
+                int next = 0;
+                if (m == -1) {
+                    next = cur - 1;
+                    if (isInRange(next) && distance[next] > distance[cur] + 1) {
+                        distance[next] = distance[cur] + 1;
+                        que.add(next);
+                    }
+                    continue;
+                }
+                if (m == 1) {
+                    next = cur + 1;
+                    if (isInRange(next) && distance[next] > distance[cur] + 1) {
+                        distance[next] = distance[cur] + 1;
+                        que.add(next);
+                    }
+                    continue;
+                }
+                if (m == 2) {
+                    next = cur * 2;
+                    if (isInRange(next) && distance[next] > distance[cur]) {
+                        distance[next] = distance[cur];
+                        que.add(next);
+                    }
+                }
+            }
+        }
+        System.out.println(distance[k]);
+    }
+
+    private static boolean isInRange(int next) {
+        return 0 <= next && next < MAX;
+    }
+}
