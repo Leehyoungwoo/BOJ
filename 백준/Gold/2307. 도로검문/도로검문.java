dc5c51ea0d13e5c44a85d79f883f448e4a6f2a4e
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-07-05 22:05:34 +0900

    [Gold I] Title: 도로검문, Time: 536 ms, Memory: 47332 KB -BaekjoonHub

---
+++ 백준/Gold/2307. 도로검문/도로검문.java
@@ -0,0 +1,132 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    private static int n;
+    private static int m;
+    private static List<List<Edge>> graph;
+    private static int answer;
+    private static int[] distance;
+    private static int[] path;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        findAnswer();
+        System.out.println(answer);
+    }
+
+    private static void init() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(input.readLine());
+        n = Integer.parseInt(tokenizer.nextToken());
+        m = Integer.parseInt(tokenizer.nextToken());
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        distance = new int[n + 1];
+        path = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            tokenizer = new StringTokenizer(input.readLine());
+            int start = Integer.parseInt(tokenizer.nextToken());
+            int end = Integer.parseInt(tokenizer.nextToken());
+            int cost = Integer.parseInt(tokenizer.nextToken());
+            graph.get(start).add(new Edge(end, cost));
+            graph.get(end).add(new Edge(start, cost));
+        }
+    }
+
+    private static void findAnswer() {
+        answer = 0;
+        int realTime = findRealTime();
+        if (realTime == Integer.MAX_VALUE) {
+            answer = -1;
+            return;
+        }
+
+        int to = n;
+        int maxDelay = 0;
+        do {
+            int from = path[to];
+            int delay = findBlockedTime(from, to);
+            if (delay == Integer.MAX_VALUE) {
+                answer = -1;
+                return;
+            }
+            maxDelay = Math.max(maxDelay, delay);
+            to = from;
+        } while (to != 1);
+
+        if (maxDelay == Integer.MAX_VALUE) {
+            answer = -1;
+        } else {
+            answer = maxDelay - realTime;
+        }
+    }
+
+    private static int findRealTime() {
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[1] = 0;
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(1, 0));
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            int cur = edge.end;
+            int cost = edge.cost;
+            if (cost > distance[cur]) continue;
+            for (Edge e : graph.get(cur)) {
+                int next = e.end;
+                int nextCost = e.cost;
+                if (distance[next] > distance[cur] + nextCost) {
+                    distance[next] = distance[cur] + nextCost;
+                    path[next] = cur;
+                    pq.offer(new Edge(next, distance[next]));
+                }
+            }
+        }
+        return distance[n];
+    }
+
+    private static int findBlockedTime(int from, int to) {
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[1] = 0;
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(1, 0));
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            int cur = edge.end;
+            int cost = edge.cost;
+            if (cost > distance[cur]) continue;
+            for (Edge e : graph.get(cur)) {
+                int next = e.end;
+                int nextCost = e.cost;
+                if ((cur == from && next == to) || (cur == to && next == from)) {
+                    continue;
+                }
+                if (distance[next] > distance[cur] + nextCost) {
+                    distance[next] = distance[cur] + nextCost;
+                    pq.offer(new Edge(next, distance[next]));
+                }
+            }
+        }
+        return distance[n];
+    }
+}
+
+class Edge implements Comparable<Edge> {
+    int end;
+    int cost;
+
+    public Edge(int end, int cost) {
+        this.end = end;
+        this.cost = cost;
+    }
+
+    @Override
+    public int compareTo(Edge o) {
+        return Integer.compare(this.cost, o.cost);
+    }
+}
