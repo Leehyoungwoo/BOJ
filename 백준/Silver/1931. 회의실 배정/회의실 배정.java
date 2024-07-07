347fcd5d26de9abfab2ca2d8b59a8d5c8d21228f
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-07-07 23:51:32 +0900

    [Silver I] Title: 회의실 배정, Time: 404 ms, Memory: 48988 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -7,12 +7,13 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        SimpleBufferedReader input = new SimpleBufferedReader(8192, new InputStreamReader(System.in));
+//        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(input.readLine());
 
         int[][] meetings = new int[N][2];
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(input.readLine());
             meetings[i][0] = Integer.parseInt(st.nextToken());
             meetings[i][1] = Integer.parseInt(st.nextToken());
         }
@@ -35,9 +36,69 @@ public class Main {
             if (meetings[i][0] >= prevEndTime) {
                 count++;
                 prevEndTime = meetings[i][1];
-            } 
+            }
         }
 
         System.out.println(count);
+    }
+}
+class SimpleBufferedReader {
+
+    private InputStreamReader inputStreamReader;
+    private char[] buffer;
+    private int bufferSize;
+    private int bufferPosition;
+    private int bufferCount;
+
+    public SimpleBufferedReader(int bufferSize, InputStreamReader inputStreamReader) {
+        this.bufferSize = bufferSize;
+        this.inputStreamReader = inputStreamReader;
+        this.buffer = new char[bufferSize];
+        this.bufferPosition = 0;
+        this.bufferCount = 0;
+    }
+
+    private void fillBuffer() throws IOException {
+        bufferCount = inputStreamReader.read(buffer, 0, bufferSize);
+        bufferPosition = 0;
+    }
+
+    public String readLine() throws IOException {
+        StringBuilder line = new StringBuilder();
+        boolean endLine = false;
+
+        while (!endLine) {
+            if (bufferPosition >= bufferCount) {
+                fillBuffer();
+                if (bufferCount == -1) {
+                    return line.length() > 0 ? line.toString() : null;
+                }
+            }
+
+            for(; bufferPosition < bufferCount; bufferPosition++) {
+                char ch = buffer[bufferPosition];
+                if (ch == '\n') {
+                    endLine = true;
+                    bufferPosition++;
+                    break;
+                } else if (ch == '\r') {
+                    endLine = true;
+                    if (bufferPosition + 1 < bufferCount && buffer[bufferPosition + 1] == '\n') {
+                        bufferPosition++;
+                    }
+                    bufferPosition++;
+                    break;
+                } else {
+                    line.append(ch);
+                }
+            }
+        }
+        return line.toString();
+    }
+
+    public void close() throws IOException {
+        if (inputStreamReader != null) {
+            inputStreamReader.close();
+        }
     }
 }
