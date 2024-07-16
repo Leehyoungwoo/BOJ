17a43cad3a8fe95d74ea117f118aa9d29113efae
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-07-16 16:00:12 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 88 ms, Memory: 13276 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static int n;
+    private static int[] nums;
+    private static int[] dp;
+    private static int[] prev;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(input.readLine());
+        nums = new int[n];
+        dp = new int[n];
+        prev = new int[n];
+
+        StringTokenizer tokenizer = new StringTokenizer(input.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+
+        Arrays.fill(dp, 1);
+        Arrays.fill(prev, -1);
+        int maxLength = 1;
+        int maxIdx = 0;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) {
+                    dp[i] = dp[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (dp[i] > maxLength) {
+                maxLength = dp[i];
+                maxIdx = i;
+            }
+        }
+
+        System.out.println(maxLength);
+        List<Integer> list = new ArrayList<>();
+        while (maxIdx != -1) {
+            list.add(nums[maxIdx]);
+            maxIdx = prev[maxIdx];
+        }
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+}
