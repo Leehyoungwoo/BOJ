366de4d4c43d67fa38b2821b923cae08e742bb8d
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-08-16 23:12:02 +0900

    [Silver III] Title: N과 M (1), Time: 184 ms, Memory: 34396 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,48 +1,43 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	private static int n;
-	private static int m;
-	private static int[] seq;
-	private static boolean[] visited;
-	
-	public static void main(String[] args) {
-		int cnt = 1;
-		init();
-		findAllSequence(cnt);
-	}
-	
-	private static void findAllSequence(int cnt) {
-		if (cnt == m + 1) {
-			printEachSeq();
-			return;
-		}
-		
-		for (int i = 1; i <= n; i++) {
-			if (!visited[i]) {
-				seq[cnt] = i;
-				visited[i] = true;
-				findAllSequence(cnt + 1);
-				visited[i] = false;	
-			}
-		}
-	}
-	
-	private static void printEachSeq() {
-		for (int i = 1; i < m + 1; i++) {
-			if (seq[i] != 0) {
-				System.out.print(seq[i] + " ");
-			}
-		}
-		System.out.println();
-	}
 
-	private static void init() {
-		Scanner input = new Scanner(System.in);
-		n = input.nextInt();
-		m = input.nextInt();
-		seq = new int[9];
-		visited = new boolean[9];
-	}
+    private static int n;
+    private static int m;
+    private static int[] nums;
+    private static StringBuilder answer = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = input.readLine().split(" ");
+        n = Integer.parseInt(s[0]);
+        m = Integer.parseInt(s[1]);
+        nums = new int[m];
+        dfs(0, new boolean[n + 1]);
+        System.out.println(answer);
+    }
+
+    private static void dfs(int idx, boolean[] visited) {
+        if (idx == m) {
+            for(Integer n : nums) {
+                answer.append(n).append(" ");
+            }
+            answer.append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                nums[idx] = i;
+                visited[i] = true;
+                dfs(idx + 1, visited);
+                visited[i] = false;
+            }
+        }
+    }
 }
