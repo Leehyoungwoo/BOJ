351c6496ddab37428f1b2fce90e8a5e9614afe50
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-08-21 15:27:39 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 568 ms, Memory: 179032 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    private static List<Integer> list;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(input.readLine());
+        list = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(input.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(tokenizer.nextToken());
+            if (list.isEmpty() || list.get(list.size() - 1) < num) {
+                list.add(num);
+            } else {
+                list.set(getLowerBound(num), num);
+            }
+        }
+        System.out.println(list.size());
+    }
+
+    private static int getLowerBound(int num) {
+        int left = 0;
+        int right = list.size();
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (list.get(mid) < num) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
+    }
+}
