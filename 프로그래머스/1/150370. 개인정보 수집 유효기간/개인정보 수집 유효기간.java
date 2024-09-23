efa7ef8fe0750f7131c13026c42d63cac0c5d61b
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-09-23 15:54:03 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 20.11 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,117 +1,46 @@
 import java.util.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 class Solution {
 
-    int[] endTimePerType;
-    String[] endTimePerPri;
+    Map<String, Integer> policy = new HashMap<>();
 
     public int[] solution(String today, String[] terms, String[] privacies) {
-        int[] answer = {};
-        endTimePerType = new int[26];
-        // 타입 별 만료기간
-        fillEndTimePerType(terms);
-        endTimePerPri = new String[privacies.length];
-        // 개인정보별 만료 기간
-        fillEndTimePerPri(privacies);
-        answer = findEndPrivacies(today, endTimePerPri, answer);
-        return answer;
-    }
-
-    private void fillEndTimePerType(String[] terms) {
+        List<Integer> list = new ArrayList<>();
+        // 약관 유효기간 저장
         for (int i = 0; i < terms.length; i++) {
             String[] s = terms[i].split(" ");
-            int idx = s[0].charAt(0) - 'A';
-            endTimePerType[idx] = Integer.parseInt(s[1]);
+            policy.put(s[0], Integer.parseInt(s[1]));
         }
-    }
 
-    private void fillEndTimePerPri(String[] privacies) {
+        // 계산해보기
         for (int i = 0; i < privacies.length; i++) {
             String[] s = privacies[i].split(" ");
-            String date = s[0];
+            String startDay = s[0];
             String type = s[1];
-            String[] days = date.split("\\.");
-            int year = Integer.parseInt(days[0]);
-            int month = Integer.parseInt(days[1]);
-            int day = Integer.parseInt(days[2]);
-            month += endTimePerType[type.charAt(0) - 'A'];
-            // 달에 관한 이슈
-            if (1 <= month && month <= 12) {
-                day--;
-                if (day == 0) {
-                    day = 28;
-                    month--;
-                    if (month == 0) {
-                        month = 12;
-                        year--;
-                    }
-                }
-            } else if (month > 12) {
-                if (month % 12 == 0) {
-                    year += (month / 12) - 1;
-                    month = 12;
-                    day--;
-                } else {
-                    year += month / 12;
-                    month %= 12;
-                    day--;
-                }
-
-                if (day == 0) {
-                    day = 28;
-                    month--;
-                    if (month == 0) {
-                        month = 12;
-                        year--;
-                    }
-                }
+            if (IsDeadline(today, startDay, type) >= 0) {
+                list.add(i + 1);
             }
-            // 타입별 만료일 기록
-            endTimePerPri[i] = year + "." + month + "." + day;
         }
+
+        return list.stream().mapToInt(i -> i).toArray();
     }
 
-    private int[] findEndPrivacies(String today, String[] endTimePerPri, int[] answer) {
-        int[] temp = new int[100];
-        int idx = 0;
-        for (int i = 0; i < endTimePerPri.length; i++) {
-            if (!validateExpired(endTimePerPri[i], today)) {
-                temp[idx] = i;
-                idx++;
-            }
-        }
-        answer = new int[idx];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = temp[i] + 1;
-        }
+    private int IsDeadline(String today, String startDay, String type) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
 
-        return answer;
+        LocalDate todayDate = LocalDate.parse(today, formatter);
+        LocalDate startDate = LocalDate.parse(startDay, formatter);
+
+        int validMonth = policy.get(type);
+
+        LocalDate lastDay = convertToLastDay(startDate, validMonth);
+
+        return todayDate.compareTo(lastDay);
     }
 
-    private boolean validateExpired(String privacy, String today) {
-        String[] str = privacy.split(" ");
-        String[] s = str[0].split("\\.");
-        int endYear = Integer.parseInt(s[0]);
-        int endMonth = Integer.parseInt(s[1]);
-        int endDay = Integer.parseInt(s[2]);
-        String[] cur = today.split("\\.");
-        int curYear = Integer.parseInt(cur[0]);
-        int curMonth = Integer.parseInt(cur[1]);
-        int curDay = Integer.parseInt(cur[2]);
-        if (endYear > curYear) {
-            return true;
-        } else if (endYear == curYear) {
-            if (endMonth > curMonth) {
-                return true;
-            } else if (endMonth == curMonth) {
-                if (endDay >= curDay) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
-
-        return false;
+    private LocalDate convertToLastDay(LocalDate startDay, int validMonth) {
+        return startDay.plusMonths(validMonth);
     }
 }
