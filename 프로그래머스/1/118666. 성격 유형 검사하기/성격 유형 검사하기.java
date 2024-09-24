27546cbc70b41a4c41aa4ce2ee7e5ebbc4c8d806
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-09-24 15:17:25 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 35.34 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,63 +1,54 @@
 import java.util.*;
 
+
 class Solution {
-    
-    int[] map;
-    char[] arr;
-    String[] globalSurvey;
-    int[] globalChoices;
-    
+
+    private final List<String> mbti = List.of("RT", "CF", "JM", "AN");
+    Map<Character, Integer> score = new HashMap<>();
+
     public String solution(String[] survey, int[] choices) {
         String answer = "";
-        init(survey, choices);
-        findAnswer();
-        answer = new String(arr);
-        return answer;
-    }
-    
-    public void init(String[] survey, int[] choices) {
-        map = new int[26];
-        arr = new char[4];
-        globalSurvey = survey;
-        globalChoices = choices;
-    }
-    
-    public void findAnswer() {
-        // MBTI 별 점수 매기기
-        for(int i = 0; i < globalSurvey.length; i++) {
-            String qa = globalSurvey[i];
-            int choice = globalChoices[i];
-            char first = qa.charAt(0);
-            char second = qa.charAt(1);
-            if (choice <= 4) {
-                map[first - 'A'] += (4 - choice);
-            } else {
-                map[second - 'A'] += (choice - 4);
+        // 점수 채점하기
+        for (int i = 0; i < survey.length; i++) {
+            String types = survey[i];
+            char first = types.charAt(0);
+            char second = types.charAt(1);
+            int response = choices[i];
+            if (response == 4) {
+                continue;
+            }
+
+            if (response < 4) {
+                score.put(first, score.getOrDefault(first, 0) + Math.abs(4 - response));
+                continue;
+            }
+
+            if (response > 4) {
+                score.put(second, score.getOrDefault(second, 0) + Math.abs(response - 4));
             }
         }
-        // 점수 계산하고 비교
-        if (map['R' - 'A'] >= map['T' - 'A']) {
-            arr[0] = 'R';
-        } else {
-            arr[0] = 'T';
+
+        // mbti 구성하기
+        for (String types : mbti) {
+            char first = types.charAt(0);
+            char second = types.charAt(1);
+            if (score.get(first) == score.get(second)) {
+                int comparison = Character.compare(first, second);
+                if (comparison < 0) {
+                    answer += first;
+                } else {
+                    answer += second;
+                }
+                continue;
+            }
+
+            if (score.getOrDefault(first, 0) > score.getOrDefault(second, 0)) {
+                answer += first;
+            } else {
+                answer += second;
+            }
         }
-        
-        if (map['C' - 'A'] >= map['F' - 'A']) {
-            arr[1] = 'C';
-        } else {
-            arr[1] = 'F';
-        }
-        
-        if (map['J' - 'A'] >= map['M' - 'A']) {
-            arr[2] = 'J';
-        } else {
-            arr[2] = 'M';
-        }
-        
-        if (map['A' - 'A'] >= map['N' - 'A']) {
-            arr[3] = 'A';
-        } else {
-            arr[3] = 'N';
-        }
+
+        return answer;
     }
 }
