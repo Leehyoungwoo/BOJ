b8f159eb5786f356eabaf66befa8b8d8d2d70013
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-11-06 17:48:50 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 68 ms, Memory: 11668 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.java
@@ -1,28 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        String regex = "c=|c-|dz=|d-|lj|nj|s=|z=";
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        String target = input.readLine();
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(target);
+    private static String s;
+    private static String regex;
 
+    public static void main(String[] args) throws IOException {
+        init();
         int count = 0;
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(s);
         while (matcher.find()) {
             count++;
         }
-
-        count += target.replaceAll("c=|c-|dz=|d-|lj|nj|s=|z=", "").length();
+        count+=s.replaceAll(regex,"").length();
 
         System.out.println(count);
+    }
+
+    private static void init() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        s = input.readLine();
+        regex = "c=|c-|dz=|d-|lj|nj|s=|z=";
     }
 }
