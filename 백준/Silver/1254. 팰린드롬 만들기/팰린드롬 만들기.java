208eae47a907818fb0775105c543440c55e80337
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-11-28 19:44:08 +0900

    [Silver II] Title: 팰린드롬 만들기, Time: 68 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.java
+++ 백준/Silver/1254. 팰린드롬 만들기/팰린드롬 만들기.java
@@ -1,22 +1,33 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        int n = s.length();
 
-        int minLength = n;  // 최소 팰린드롬 길이 (초기값 = 원래 길이)
+    private static String word;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        findAnswer();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        word = input.readLine();
+    }
+
+    private static void findAnswer() {
+        int n = word.length();
+        int midLength = n;
         for (int i = 0; i < n; i++) {
-            if (isPalindrome(s.substring(i))) {
-                minLength = n + i;  // 앞의 i 문자들을 추가한 길이로 팰린드롬 가능
+            if (isPalindrome(word.substring(i))) {
+                midLength = n + i;
                 break;
             }
         }
-        System.out.println(minLength);
+
+        System.out.println(midLength);
     }
 
-    // 팰린드롬 판별 함수
     private static boolean isPalindrome(String str) {
         int left = 0, right = str.length() - 1;
         while (left < right) {
