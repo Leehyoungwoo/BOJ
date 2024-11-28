c926f32c80f449f449713f2782a9954f86ea6b60
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2024-11-28 21:19:13 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.22 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,66 +1,75 @@
 import java.util.*;
 
 class Solution {
-    private Queue<int[]> que = new LinkedList<>();
-    private final int[][] direction = {{1,0},{-1,0}, {0, 1}, {0, -1}};
-
+    
+    private final int[][] direction = {{1,0}, {-1, 0}, {0, 1}, {0, -1}};
+    
     public int[] solution(String[][] places) {
-        int[] answer = new int[5];
-
-        for (int i = 0; i < 5; i++) {
-            char[][] map = new char[5][5];
-            for (int j = 0; j < 5; j++) {
-                map[j] = places[i][j].toCharArray();
-            }
-            answer[i] = isKeepDistance(map) ? 1 : 0;
-            que.clear();
+        int[] answer = new int[places.length];
+        for (int i = 0; i < places.length; i++) {
+            answer[i] = findAnswer(places[i]);
         }
-
         return answer;
     }
-
-    public boolean isKeepDistance(char[][] map) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
+    
+    private int findAnswer(String[] place) {
+        char[][] map = new char[place.length][place.length];
+        for (int i = 0; i < map.length; i++) {
+            map[i] = place[i].toCharArray();
+        }
+        
+        if (isGoodDistance(map)) {
+            return 1;
+        }
+        
+        return 0;
+    }
+    
+    private boolean isGoodDistance(char[][] map) {
+        for (int i = 0; i < map.length; i++) {
+            for(int j = 0; j < map.length; j++) {
                 if (map[i][j] == 'P') {
-                    if (!isSafe(map, i, j)) {
+                    if (!isDistance2ToAllPeople(i, j, map)) {
                         return false;
                     }
                 }
             }
         }
+        
         return true;
     }
-
-    public boolean isSafe(char[][] map, int r, int c) {
-        boolean[][] visited = new boolean[5][5];
-        que.clear();
-        que.offer(new int[]{r, c, 0});
+    
+    private boolean isDistance2ToAllPeople(int r, int c, char[][] map) {
+        Queue<int[]> que = new LinkedList<>();
+        int size = map.length;
+        boolean[][] visited = new boolean[size][size];
+        que.offer(new int[] {r, c, 0});
         visited[r][c] = true;
-
-        while (!que.isEmpty()) {
+        while(!que.isEmpty()) {
             int[] cur = que.poll();
             int curR = cur[0];
             int curC = cur[1];
-            int curK = cur[2];
-
-            if (curK >= 2) {
+            int distance = cur[2];
+            if (distance >= 2) {
                 continue;
             }
-
             for (int[] dir : direction) {
                 int nextR = curR + dir[0];
                 int nextC = curC + dir[1];
-                if (nextR >= 0 && nextR < 5 && nextC >= 0 && nextC < 5 && !visited[nextR][nextC]) {
+                if (isInRange(nextR, nextC, size) && map[nextR][nextC] != 'X' &&
+                   !visited[nextR][nextC]) {
                     if (map[nextR][nextC] == 'P') {
                         return false;
-                    } else if (map[nextR][nextC] == 'O') {
-                        que.offer(new int[]{nextR, nextC, curK + 1});
-                        visited[nextR][nextC] = true;
                     }
+                    que.offer(new int[] {nextR, nextC, distance + 1});
+                    visited[nextR][nextC] = true;
                 }
             }
         }
         return true;
+    }
+    
+    private boolean isInRange(int r, int c, int n) {
+        return 0 <= r && r < n && 0 <= c && c < n;
     }
 }
