1015ff512636bf4a0d83c4616040473579157b80
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-01-08 21:34:49 +0900

    [Bronze V] Title: 고양이, Time: 72 ms, Memory: 11904 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.kt
@@ -0,0 +1,10 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.StringTokenizer
+
+fun main(args: Array<String>) {
+    println("\\    /\\")
+    println(" )  ( ')")
+    println("(  /  )")
+    println(" \\(__)|")
+}
