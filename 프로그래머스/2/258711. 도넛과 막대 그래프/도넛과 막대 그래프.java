b538b11960b3fafca735828259660daf2d83f022
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-02-26 21:24:49 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 601.45 ms, Memory: 378 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -3,46 +3,44 @@ import java.util.*;
 class Solution {
     public int[] solution(int[][] edges) {
         int[] answer = new int[4];
-        // [0]은 나가는거 [1]은 들어가는거
-        Map<Integer, int[]> map = new HashMap<>();
-
-        // n개의 정점과 n개의 간선
-        // 노드의 수
-        Set<Integer> set = new HashSet<>();
-
-        for (int i = 0; i < edges.length; i++) {
-            int[] temp = map.getOrDefault(edges[i][0], new int[2]);
+        // node 저장
+        Set<Integer> nodes = new HashSet<>();
+        // [0][0]은 나가는거, [0][1]은 들어오는거
+        Map<Integer, int[]> count = new HashMap<>();
+        for(int i = 0; i < edges.length; i++) {
+            int from = edges[i][0];
+            int to = edges[i][1];
+            int[] temp = count.getOrDefault(from, new int[2]);
             temp[0]++;
-            map.put(edges[i][0], temp);
-            set.add(edges[i][0]);
-
-            int[] temp2 = map.getOrDefault(edges[i][1], new int[2]);
+            count.put(from, temp);
+            nodes.add(from);
+            
+            int[] temp2 = count.getOrDefault(to, new int[2]);
             temp2[1]++;
-            map.put(edges[i][1], temp2);
-            set.add(edges[i][1]);
+            count.put(to, temp2);
+            nodes.add(to);
         }
-        int nodeCount = set.size();
-        for (Integer i : set) {
-            //도넛 모양 그래프, 막대 모양 그래프, 8자 모양 그래프의 수의 합은 2이상이면서 들어오는 간선이 없으면 정점
-            if (map.get(i)[0] >= 2 && map.get(i)[1] == 0) {
+        int nodeCount = nodes.size();
+        for (Integer i : nodes) {
+//도넛 모양 그래프, 막대 모양 그래프, 8자 모양 그래프의 수의 합은 2이상이면서 들어오는 간선이 없으면 정점
+            if (count.get(i)[0] >= 2 && count.get(i)[1] == 0) {
                 answer[0] = i;
                 continue;
             }
-
+            
             // 나가는 간선이 없으면 직선 그래프
-            if (map.get(i)[0] == 0 && map.get(i)[1] >= 1) {
+            if (count.get(i)[0] == 0) {
                 answer[2]++;
                 continue;
-            }
-
-            // 8자형 그래프
-            if (map.get(i)[0] >= 2 && map.get(i)[1] >= 2) {
+            } 
+            
+            // 8자형 그래프는 들어오는게 2, 나가는게 2인 노드가 있으면 존재
+            if (count.get(i)[0] >= 2 && count.get(i)[1] >= 2) {
                 answer[3]++;
             }
         }
-
-        // 총 그래프의 개수는 정점에서 나가는 간선의 개수
-        answer[1] = map.get(answer[0])[0] - answer[2] - answer[3];
+        // 총 새구는 정점에서 나가는 간선의 개수 - 나머지 그래프 = 도넛형 그래프
+        answer[1] = count.get(answer[0])[0] - answer[2] - answer[3];
         return answer;
     }
 }
