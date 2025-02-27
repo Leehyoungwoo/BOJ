1c7e59df65842936cd188af070765131d4ab03d3
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-02-27 22:35:47 +0900

    [level 3] Title: 산 모양 타일링, Time: 8.33 ms, Memory: 94.1 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,25 +1,30 @@
-import java.util.*;
-
+// 삼각
 class Solution {
-    
-    private final int DIV_NUM = 10_007;
-    
     public int solution(int n, int[] tops) {
-        int[] a = new int[n + 1];
-        int[] b = new int[n + 1];
-
+        final int MOD = 10007;
+        
+        int[] a = new int[n + 1]; 
+        int[] b = new int[n + 1]; 
+        
+        // dp 초기화
         a[1] = 1;
-        if (tops[0] == 1) b[1] = 3;
-        else b[1] = 2;
-
-        for (int i = 2; i <= n; i++) {
-            a[i] = (a[i - 1] + b[i - 1]) % DIV_NUM;
+        if (tops[0] == 1) {
+            // 세가지 모양이 들어가니까
+            b[1] = 3;
+        } else {
+            b[1] = 2;
+        }
+        
+        for(int i = 2; i < n + 1; i++) {
+            a[i] = (a[i - 1] + b[i - 1]) % MOD;
+            // 위에 삼각형이 있을 경우 top[i - 1] == 1;
             if (tops[i - 1] == 1) {
-                b[i] = (a[i - 1] * 2 + b[i - 1] * 3) % DIV_NUM;
+                b[i] = (a[i - 1] * 2 + b[i - 1] * 3) % MOD;
             } else {
-                b[i] = (a[i - 1] + b[i - 1] * 2) % DIV_NUM;
+                b[i] = (a[i - 1] + b[i - 1] * 2) % MOD;
             }
         }
-        return (a[n] + b[n]) % DIV_NUM;
+        
+        return (a[n] + b[n]) % MOD;
     }
 }
