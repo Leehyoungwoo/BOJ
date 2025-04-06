9e0430df9edbde5c2afac20fcb9b28fcacc50189
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-04-06 19:22:13 +0900

    [level 3] Title: 산 모양 타일링, Time: 5.54 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.java
@@ -1,4 +1,5 @@
-// 삼각
+// 삼각형 두개가 겹치는 부분이 어떤 마름모 모양이냐에 따라서 다음에 들어갈 수 있는 도형이 결정남
+// 두개의 디피 배열로 나누고 top이 있는 경우와 없는 경우를 고려하면 됨
 class Solution {
     public int solution(int n, int[] tops) {
         final int MOD = 10007;
