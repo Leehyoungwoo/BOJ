c682c1230358c02c91c1770bd32281292d7c1c01
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-08-07 20:51:46 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n = (long) num;
+        while (n != 1) {
+            if (answer == 500) {
+                return -1;
+            }
+            if (n % 2 == 0) {
+                n/=2;
+            } else {
+                n *=3;
+                n++;
+            }
+            
+            answer++;
+        }
+        return answer;
+    }
+}
