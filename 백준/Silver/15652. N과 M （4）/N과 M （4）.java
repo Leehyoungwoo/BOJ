8e3c2cd0e67dadb91ad0cf8191d58a2b1f7a9765
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-08-20 13:45:46 +0900

    [Silver III] Title: N과 M (4), Time: 92 ms, Memory: 14784 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -4,54 +4,29 @@ import java.util.*;
 public class Main {
 
     private static int n, m;
-    private static StringBuilder answer;
-    private static Set<String> set;
     private static int[] comb;
+    private static StringBuilder out = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        init();
-        findAnswer();
-    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
 
-    private static void init() throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(input.readLine());
-        n = Integer.parseInt(tokenizer.nextToken());
-        m = Integer.parseInt(tokenizer.nextToken());
-        answer = new StringBuilder();
         comb = new int[m];
-        set = new HashSet<>();
+        dfs(0, 1); 
+        System.out.print(out);
     }
 
-    private static void findAnswer() {
-        dfs(0, 1);
-        List<String> list = new ArrayList<>(set);
-        Collections.sort(list);
-        StringBuilder sb = new StringBuilder();
-        for (String str : list) {
-            sb.append(str);
-        }
-        System.out.println(sb);
-    }
-
-    private static void dfs(int idx, int start) {
-        if (idx == m) {
-            answer = new StringBuilder();
-            for (int i = 0; i < comb.length; i++) {
-                answer.append(comb[i]).append(" ");
-            }
-            answer.append("\n");
-            set.add(answer.toString());
+    private static void dfs(int depth, int start) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) out.append(comb[i]).append(' ');
+            out.append('\n');
             return;
         }
-
-        if (start == n + 1) {
-            return;
+        for (int i = start; i <= n; i++) {
+            comb[depth] = i;
+            dfs(depth + 1, i); 
         }
-
-        comb[idx] = start;
-        dfs(idx + 1, start);
-        dfs(idx + 1, start + 1);
-        dfs(idx, start + 1);
     }
 }
