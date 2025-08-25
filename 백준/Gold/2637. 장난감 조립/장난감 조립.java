004c6bb67e5e3a419df498eb72661ef2ea964818
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-08-25 17:46:03 +0900

    [Gold II] Title: 장난감 조립, Time: 68 ms, Memory: 11604 KB -BaekjoonHub

---
+++ 백준/Gold/2637. 장난감 조립/장난감 조립.java
@@ -0,0 +1,89 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    // 1~ n-1은 부품, n은 완제품 번호
+    private static int n;
+    private static int m;
+    private static int made;
+    private static List<List<int[]>> graph = new ArrayList<>();
+    // 부품을 만드는데 필요한 관계 x, y, k는  X를 만드는데 y가 k개 필요하다
+
+    public static void main(String[] args) throws IOException {
+        init();
+        findAnswer();
+    }
+
+    private static void init() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(input.readLine());
+        m = Integer.parseInt(input.readLine());
+        for (int i = 0; i <= n; i++) {
+            List<int[]> list = new ArrayList<>();
+            graph.add(list);
+        }
+
+        int low = Integer.MAX_VALUE;
+        for (int i = 0; i < m; i++) {
+            String[] s = input.readLine().split(" ");
+            int number = Integer.parseInt(s[0]);
+            int puzzle = Integer.parseInt(s[1]);
+            int count = Integer.parseInt(s[2]);
+            low = Math.min(low, number);
+            graph.get(number).add(new int[] {puzzle, count});
+        }
+
+        made = low - 1;
+    }
+
+    private static void findAnswer() {
+        StringBuilder answer = new StringBuilder();
+        int[] need = new int[n + 1];
+        make(n, 1, need);
+
+        for (int i = 1; i < n; i++) {                
+            if (graph.get(i).isEmpty() && need[i] > 0) {  
+                answer.append(i).append(" ").append(need[i]).append("\n");
+            }
+        }
+
+        System.out.println(answer);
+    }
+
+    private static void make(int product, int quantity, int[] need) {
+        int partCount = graph.size() - 1;
+        int[] required = new int[partCount + 1];
+        boolean[] inQueue = new boolean[partCount + 1];
+        Queue<Integer> queue = new LinkedList<>();
+
+        required[product] = quantity;
+        queue.offer(product);
+        inQueue[product] = true;
+
+        while (!queue.isEmpty()) {
+            int part = queue.poll();
+            inQueue[part] = false;
+
+            int amount = required[part];
+            required[part] = 0;
+            if (amount == 0) continue;
+
+            if (graph.get(part).isEmpty()) {
+                need[part] += amount;
+                continue;
+            }
+
+            for (int[] sub : graph.get(part)) {
+                int subPart = sub[0];
+                int subCount = sub[1];
+                required[subPart] += amount * subCount;
+
+                if (!inQueue[subPart]) {
+                    queue.offer(subPart);
+                    inQueue[subPart] = true;
+                }
+            }
+        }
+    }
+}
