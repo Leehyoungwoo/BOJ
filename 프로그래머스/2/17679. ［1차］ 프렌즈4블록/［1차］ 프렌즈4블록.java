1c99b74767b51c694aa9ceb251a99fd3692b2e4a
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-10-06 21:20:01 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 8.61 ms, Memory: 94 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.java
@@ -45,11 +45,19 @@ class Solution {
 
     private void drop() {
         for (int c = 0; c < col; c++) {
-            int write = row - 1; 
+            int write = row - 1;
             for (int r = row - 1; r >= 0; r--) {
+                // 아래부터 올라가면서 빈칸이 아닌거 확인
                 if (map[r][c] != '.') {
+                    // 빈칸이 아닌걸 write 포인터에 넣고 포인터 증가
                     map[write][c] = map[r][c];
-                    if (write != r) map[r][c] = '.';
+                    // write는 넣어야할 곳, r은 넣어야할 것(빈칸이 아닌것)
+                    // 넣어야할 곳에 넣어야할 것이 들어가면 write 한칸 위로 --
+                    // 빈칸을 스킵하면서 압축시키기. 넣어야할 것은 빈칸을 스킵하고 넣어야할 곳에 넣어주면 됨. 전부 빈칸이 아니라면 그대로 제자리에 들어가게 될 것
+//빈칸이 있다면 write인 빈칸에 다음 문자인 r이 들어가는거지
+                    if (write != r) {
+                        map[r][c] = '.';
+                    }
                     write--;
                 }
             }
