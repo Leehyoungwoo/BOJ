f3e84ec7486d100f86077080fabe61ff0a2f0a13
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-10-23 19:15:17 +0900

    [Silver IV] Title: 수 찾기, Time: 420 ms, Memory: 51048 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,59 +1,59 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    private static int n;
-    private static int m;
-    private static int[] arr;
-    private static Queue<Integer> que = new LinkedList<>();
+    private static int n, m;
+    private static int[] num;
+    private static int[] order;
 
     public static void main(String[] args) throws IOException {
         init();
-        StringBuilder answer = new StringBuilder();
-        for (int i = 0; i < m; i++) {
-            int target = que.poll();
-            int num = findAnswer(target);
-            answer.append(num).append("\n");
-        }
-        System.out.println(answer);
-    }
-
-    private static int findAnswer(int target) {
-        int left = 0;
-        int right = arr.length - 1;
-        while (left<=right) {
-            int mid = (left + right) / 2;
-            if(arr[mid] < target) {
-                left = mid + 1;
-                continue;
-            }
-
-            if(arr[mid] > target) {
-                right = mid - 1;
-                continue;
-            }
-            return 1;
-        }
-
-        return 0;
+        findAnswer();
     }
 
     private static void init() throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(input.readLine());
-        arr = new int[n];
+        num = new int[n];
         StringTokenizer tokenizer = new StringTokenizer(input.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(tokenizer.nextToken());
+            num[i] = Integer.parseInt(tokenizer.nextToken());
         }
-        Arrays.sort(arr);
         m = Integer.parseInt(input.readLine());
         tokenizer = new StringTokenizer(input.readLine());
+        order = new int[m];
         for (int i = 0; i < m; i++) {
-            que.offer(Integer.parseInt(tokenizer.nextToken()));
+            order[i] = Integer.parseInt(tokenizer.nextToken());
         }
+    }
+
+    private static void findAnswer() {
+        Arrays.sort(num);
+        StringBuffer answer = new StringBuffer();
+        for (int i = 0; i < m; i++) {
+            int target = order[i];
+            answer.append(binarySearch(target)).append("\n");
+        }
+
+        System.out.println(answer);
+    }
+
+    private static int binarySearch(int target) {
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (num[mid] == target) {
+                return 1;
+            }
+            if (num[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return 0;
     }
 }
