8449333dc6292e2c629206d8c6a476d0d5851829
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-10-25 23:00:26 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 52.12 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,76 +1,77 @@
 import java.util.*;
 
 class Solution {
+    // 이모티콘 플러스 서비스 가입자 수 늘리기
+    // 가입자 늘리기, 판매액 늘리기 -> 1번이 우선 2번이 그 다음
+    // n명에게 m개를 할인해서 판매 10, 20, 30, 40중 하나
+    // 사람들은 기준이 있음
+    // 일정 비율 할인하는거 모두 구매, 구매 비용 합이 일정 가격을 넘어가면 모두 취소하고 플러스 가입
+    // 우리 기준은 가입자 선, 그리고 만약에 같으면 판매액
+    // 이모티콘 어떤 할인을 넣을까 순열을 구해야하는데 7개고 할인률 4가지니가 2^14 = 만육천? 충분
+    // 중복 순열을 구하고 모든 할인율 적용한 가격표와 통합 금액을 만들어서 가입자 수와 매출 기록
+    private final int[] rate = {40, 30, 20, 10};
+    private int[][] policy;
+    private int[] price;
+    // 순열을 담자
+    private int[] per;
+    private int maxPeople = Integer.MIN_VALUE;
+    private int maxSale = Integer.MIN_VALUE;
+    private int n;
     
-    private final int[] percentages = {10, 20, 30, 40};
-    private int[] comb;
-    private int maxUser = Integer.MIN_VALUE;
-    private int maxSales = Integer.MIN_VALUE;
-
     public int[] solution(int[][] users, int[] emoticons) {
-        comb = new int[emoticons.length];
-        // 자신의 기준에 따라 일정 비율 이상 할인하는 이모티콘 모두 구매
-        makeComb(0, users, emoticons);
-        // 구매 비용의 합이 일정 가격 이상이 되면 이모티콘 구매를 취고하고 플러스에 가입
+        // 초기화부터 하자
+        init(users, emoticons);
+        // 일단 이모티콘의 중복 순열부터 구하자
+        makePermutation(0);
         
-        // 목표는 가입자 최대한 늘리기 (최우선) 판매액 최대한 늘리기
-        
-        // 가입자 수와 매출액 1차원 정수 배열에 담아 return
-        return new int[] {maxUser, maxSales};
+        return new int[] {maxPeople, maxSale};
     }
     
-    private void makeComb(int idx, int[][] users, int[] emoticons) {
-        if (idx == emoticons.length) {
-            // for(int i = 0; i < comb.length; i++) {
-            //     System.out.print(comb[i] + " ");
-            // }
-            // System.out.println();
-            int[] salesEmoticons = addSalePriceToEmoticons(emoticons);
-            int salseSsum = 0;
-            int plusUser = 0;
-            for (int i = 0; i < users.length; i++) {
-                int sales = 0;
-                int userNeedsPercent = users[i][0];
-                int userNeedsOverPrice = users[i][1];
-                for (int j = 0; j < emoticons.length; j++) {
-                    if (userNeedsPercent <= percentages[comb[j]]) {
-                        sales+=salesEmoticons[j];
+    private void makePermutation(int idx) {
+        if (idx == n) {
+            // per에 할인율 중복 순열 들어가 있음
+            // 사용자는 할인율을 기준으로 선택함
+            // 모든 사용자가 모든 이모티콘을 순회하면서 할인욜보다 높아? 그럼 get 그러다가 total이 예산을 넘으면 환불하고 플러스 가입
+            int customer = 0;
+            int total = 0;
+            for (int i = 0; i < policy.length; i++) {
+                int priceSum = 0;
+                for (int j = 0; j < n; j++) {
+                    int saleRate = rate[per[j]];
+                    // 할인율이 기준을 충족하면 구매
+                    if (saleRate >= policy[i][0]) {
+                        int p = (100 - saleRate) * price[j] / 100;
+                        priceSum+=p;
                     }
-                    // 만약 sales가 userNeedsOverPrice를 넘으면 반품하고 플러스 가입
-                    if (sales >= userNeedsOverPrice) {
-                        sales = 0;
-                        plusUser++;
-                        break;
-                    }
+                    
+                    // 근데 금액이 예산 이상이면 이제 환불
                 }
-                salseSsum+=sales;
+                if (priceSum >= policy[i][1]) {
+                        priceSum = 0;
+                        customer++;
+                }
+                total+=priceSum;
+            }
+            if (maxPeople < customer) {
+                maxPeople = customer;
+                maxSale = total;
+            } else if (maxPeople == customer) {
+                maxSale = Math.max(maxSale, total);
             }
             
-            if (plusUser > maxUser) {
-                maxUser = plusUser;
-                maxSales = salseSsum;
-            }
-            
-            if (plusUser == maxUser) {
-                if (salseSsum > maxSales) {
-                    maxSales = salseSsum;
-                }
-            }
             return;
         }
+        
         for (int i = 0; i < 4; i++) {
-            comb[idx] = i;
-            makeComb(idx + 1, users, emoticons);
+            per[idx] = i;
+            makePermutation(idx + 1);
         }
     }
     
-    private int[] addSalePriceToEmoticons(int[] emoticons) {
-        int[] salePrice = new int[emoticons.length];
-        for(int i = 0; i < salePrice.length; i++) {
-            // 세일가 = 원가 * (100 - 세일퍼센트) / 100;
-            salePrice[i] = emoticons[i] * (100 - percentages[comb[i]]) / 100;
-        }
-        
-        return salePrice;
+    private void init(int[][] users, int[] emoticons) {
+        policy = users;
+        price = emoticons;
+        per = new int[price.length];
+        n = price.length;
     }
 }
