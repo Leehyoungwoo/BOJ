73838300ac8d7b104e038d69c9948bc768fb37b9
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-11-03 14:06:17 +0900

    [Gold V] Title: 암호 만들기, Time: 80 ms, Memory: 11640 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,99 +1,72 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-	private static int L;
-	private static int C;
-	private static char[] ch;
-	private static ArrayList<Character> consonants = new ArrayList<>();
-	private static char[] temp;
-	private static int[] idxarr;
 
-	public static void main(String[] args) throws IOException {
-		init();
-		findPassword(0, 0);
-	}
+    private static int l;
+    private static int c;
+    private static char[] alphabet;
+    private static Set<Character> set;
+    private static char[] string;
+    private static StringBuilder answer;
 
-	private static void findPassword(int idx, int start) {
-		if (idx == L) {
-			temp = makingTemp();
-			if(!isValidArr(temp)) {
-				return;
-			}
-			printTemp(temp);
-			System.out.println();
-			return;
-		}
-		
-		if (start == C) {
-			return;
-		}
-		
-		idxarr[idx] = start;
-		findPassword(idx + 1, start + 1);
-		idxarr[idx] = -1;
-		findPassword(idx, start + 1);
-	}
+    public static void main(String[] args) throws IOException {
+        init();
+        String answer = findAnswer();
 
-	private static boolean isValidArr(char[] temp2) {
-		int consonantsCnt = 0;
-		int vowelsCnt = 0;
-		
-		for (int i = 0; i < temp.length; i++) {
-			if(consonants.contains(temp[i])) {
-				consonantsCnt++;
-				continue;
-			}
-			vowelsCnt++;
-		}
-		
-		if(vowelsCnt >=2 && consonantsCnt >= 1) {
-			return true;
-		}
-		
-		return false;
-	}
+        System.out.println(answer);
+    }
 
-	private static void printTemp(char[] temp) {
-		for (int i = 0; i < temp.length; i++) {
-			System.out.print(temp[i]);
-		}
-	}
+    private static void init() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(input.readLine());
+        l = Integer.parseInt(tokenizer.nextToken());
+        c = Integer.parseInt(tokenizer.nextToken());
+        String[] in = input.readLine().split(" ");
+        alphabet = new char[c];
+        string = new char[l];
+        for (int i = 0; i < c; i++) {
+            alphabet[i] = in[i].charAt(0);
+        }
+    }
 
-	private static char[] makingTemp() {
-		char[] temp = new char[L];
-		
-		for (int i = 0; i < idxarr.length; i++) {
-			temp[i] = ch[idxarr[i]];
-		}
-		
-		return temp;
-	}
+    private static String findAnswer() {
+        answer = new StringBuilder();
+        // 우선 알파벳 순으로 정렬
+        Arrays.sort(alphabet);
+        // 모음 Set 만들어놓자
+        set = new HashSet<>();
+        set.addAll(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        // 최소 한개의 모음과 최소 두 개의 자음으로 l길이의 문자열을 만들어야함, String을 매 재귀에 호출해도 될거 같긴 한데 굳이 그러지말자
+        // 카운터를 관리하고, visited도 써야할듯
+        makeWord(0, 0, 0, 0);
 
-	private static void init() throws IOException {
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer tokenizer = new StringTokenizer(input.readLine());
-		L = Integer.parseInt(tokenizer.nextToken());
-		C = Integer.parseInt(tokenizer.nextToken());
-		temp = new char[L];
-		ch = new char[C];
-		idxarr = new int[L];
-		StringTokenizer tokenizer2 = new StringTokenizer(input.readLine());
-		
-		for (int i = 0; i < C; i++) {
-			ch[i] = tokenizer2.nextToken().charAt(0);
-		}
-		
-		Arrays.sort(ch);
-		consonants.add('a');
-		consonants.add('e');
-		consonants.add('i');
-		consonants.add('o');
-		consonants.add('u');
-	}
+        return answer.toString();
+    }
+
+    private static void makeWord(int idx, int start, int 모음카운터, int 자음카운터) {
+        if (idx == l) {
+            if (모음카운터 < 1 || 자음카운터 < 2) {
+                return;
+            }
+            answer.append(new String(string)).append("\n");
+            return;
+        }
+
+        if (start == c) {
+            return;
+        }
+
+        if (set.contains(alphabet[start])) {
+            // 모듬 케이스
+            string[idx] = alphabet[start];
+            makeWord(idx + 1, start + 1, 모음카운터 + 1, 자음카운터);
+            makeWord(idx, start + 1, 모음카운터 , 자음카운터);
+        } else {
+            // 자음
+            string[idx] = alphabet[start];
+            makeWord(idx + 1, start + 1, 모음카운터, 자음카운터 + 1);
+            makeWord(idx, start + 1, 모음카운터 , 자음카운터);
+        }
+    }
 }
