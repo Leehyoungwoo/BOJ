878545cf7e40b42df605cd282cb1e5af942e3400
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-11-04 22:56:15 +0900

    [Gold II] Title: 문제집, Time: 396 ms, Memory: 45796 KB -BaekjoonHub

--- 백준/Gold/1766. 문제집/문제집.java
+++ 백준/Gold/1766. 문제집/문제집.java
@@ -3,50 +3,56 @@ import java.util.*;
 
 public class Main {
 
+    private static int n;
+    private static int m;
+    private static int[] indegree;
+    private static List<List<Integer>> graph;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(input.readLine());
+        init();
+        findAnswer();
+    }
 
-        int n = Integer.parseInt(tokenizer.nextToken());
-        int m = Integer.parseInt(tokenizer.nextToken());
-
-        List<List<Integer>> graph = new ArrayList<>();
-        int[] indegree = new int[n + 1];
-
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < m; i++) {
-            tokenizer = new StringTokenizer(input.readLine());
-            int a = Integer.parseInt(tokenizer.nextToken());
-            int b = Integer.parseInt(tokenizer.nextToken());
-            graph.get(a).add(b);
-            indegree[b]++;
-        }
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
+    private static void findAnswer() {
+        StringBuilder answer = new StringBuilder();
+        PriorityQueue<Integer> que = new PriorityQueue<>();
         for (int i = 1; i <= n; i++) {
             if (indegree[i] == 0) {
-                pq.offer(i);
+                que.offer(i);
             }
         }
 
-        StringBuilder result = new StringBuilder();
-
-        while (!pq.isEmpty()) {
-            int current = pq.poll();
-            result.append(current).append(" ");
-
-            for (int next : graph.get(current)) {
+        while (!que.isEmpty()) {
+            int cur = que.poll();
+            answer.append(cur).append(" ");
+            for (int next : graph.get(cur)) {
                 indegree[next]--;
                 if (indegree[next] == 0) {
-                    pq.offer(next);
+                    que.offer(next);
                 }
             }
         }
 
-        System.out.println(result);
+        System.out.println(answer);
+    }
+
+    private static void init() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(input.readLine());
+
+        n = Integer.parseInt(tokenizer.nextToken());
+        m = Integer.parseInt(tokenizer.nextToken());
+        indegree = new int[n + 1];
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m; i++) {
+            tokenizer = new StringTokenizer(input.readLine());
+            int a = Integer.parseInt(tokenizer.nextToken());
+            int b = Integer.parseInt(tokenizer.nextToken());
+            indegree[b]++;
+            graph.get(a).add(b);
+        }
     }
 }
