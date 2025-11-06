4692cc0a1b768e007fc8b3ea4206f04679d1f512
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-11-06 09:58:14 +0900

    [level 3] Title: 입국심사, Time: 91.51 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,30 +2,27 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
         Arrays.sort(times);
-        long left = 1;
-        long right = (long)n * times[0];
-        while (left <= right) {
-            long mid = (right + left) / 2;
-            if (canPlace(mid, times, n)) {
-                answer = mid;
-                right = mid - 1;
-            } else {
+        long left = 0;
+        long right = (long) times[0] * n;        
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            if (isValid(mid, n, times)) {
                 left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-        return answer;
+        
+        return left;
     }
     
-    private boolean canPlace(long mid, int[] times, int n) {
+    private boolean isValid(long target, int n, int[] times) {
         long sum = 0;
-        for(int time : times) {
-            sum += mid / time;
-            if(sum >= n) {
-                return true;
-            }
+        for (int time : times) {
+            sum+=target / time;
         }
-        return sum >= n;
+        
+        return sum < n;
     }
 }
