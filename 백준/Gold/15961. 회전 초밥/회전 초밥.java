417a949c9d718e4568d4e7836e753d522fdf3bbc
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-11-17 15:51:53 +0900

    [Gold IV] Title: 회전 초밥, Time: 480 ms, Memory: 168936 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -1,77 +1,69 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    private static int N;
-    private static int d;
-    private static int k;
-    private static int c;
-    private static ArrayList<Integer> susi;
-    private static Set<Integer> window;
-    
+
+    private static int n, d, k, c;
+    private static int[] sushi;
+
     public static void main(String[] args) throws IOException {
         init();
-        int answer = findEat();
-        System.out.println(answer);
-    }
+        int answer = findAnswer();
 
-    private static int findEat() {
-        int maxNum = 0;
-        int[] count = new int[d + 1];
-        
-        for (int i = 0; i < k; i++) {
-            int sushiType = susi.get(i);
-            if (count[sushiType] == 0) {
-                window.add(sushiType);
-            }
-            count[sushiType]++;
-        }
-        
-        maxNum = window.size();
-        
-        for (int i = 1; i < N; i++) {
-            int prevSushi = susi.get(i - 1);
-            int newSushi = susi.get((i + k - 1) % N);
-            
-            count[prevSushi]--;
-            if (count[prevSushi] == 0) {
-                window.remove(prevSushi);
-            }
-            
-            if (count[newSushi] == 0) {
-                window.add(newSushi);
-            }
-            count[newSushi]++;
-            
-            if (count[c] == 0) {
-                maxNum = Math.max(maxNum, window.size() + 1);
-            } else {
-                maxNum = Math.max(maxNum, window.size());
-            }
-        }
-        
-        return maxNum;
+        System.out.println(answer);
     }
 
     private static void init() throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokenizer = new StringTokenizer(input.readLine());
-        N = Integer.parseInt(tokenizer.nextToken());
+        n = Integer.parseInt(tokenizer.nextToken());
         d = Integer.parseInt(tokenizer.nextToken());
         k = Integer.parseInt(tokenizer.nextToken());
         c = Integer.parseInt(tokenizer.nextToken());
-        susi = new ArrayList<>();
-        window = new HashSet<>();
-        
-        for (int i = 0; i < N; i++) {
-            tokenizer = new StringTokenizer(input.readLine());
-            int a = Integer.parseInt(tokenizer.nextToken());
-            susi.add(a);
+        sushi = new int[n];
+        for (int i = 0; i < n; i++) {
+            sushi[i] = Integer.parseInt(input.readLine());
         }
+    }
+
+    private static int findAnswer() {
+        // 윈도우를 구성하고
+        int count = 0;
+        int[] counts = new int[d + 1];
+        for (int i = 0; i < k; i++) {
+            if (counts[sushi[i]] == 0) {
+                count++;
+            }
+            counts[sushi[i]]++;
+        }
+
+        int max = 0;
+        int left = 0;
+        int right = k - 1;
+        while (left < sushi.length) {
+            counts[sushi[left]]--;
+            if (counts[sushi[left]] == 0) {
+                count--;
+            }
+            left++;
+
+            right++;
+            if (right == sushi.length) {
+                right = 0;
+            }
+
+            counts[sushi[right]]++;
+            if (counts[sushi[right]] == 1) {
+                count++;
+            }
+
+            if (counts[c] == 0) {
+                max = Math.max(max, count + 1);
+            }  else {
+                max = Math.max(max, count);
+            }
+        }
+
+        return max;
     }
 }
