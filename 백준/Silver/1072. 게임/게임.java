2beb17cc7bba3dbd26ce4e69ae1d057b99821dc2
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-12-03 16:56:58 +0900

    [Silver III] Title: 게임, Time: 64 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.java
+++ 백준/Silver/1072. 게임/게임.java
@@ -1,33 +1,47 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
+import java.math.*;
 
 public class Main {
 
-    private static long X;
-    private static long Y;
+    private static int x, y;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = input.readLine().split(" ");
-        X = Long.parseLong(s[0]);
-        Y = Long.parseLong(s[1]);
-        int Z = (int) ((100 * Y) / X);
-        if (Z >= 99) {
-            System.out.println(-1);
-            return;
-        }
-        long left = 0;
-        long right = 1000000000L;
+        init();
+        int answer = findAnswer();
 
+        System.out.println(answer);
+    }
+
+    private static int findAnswer() {
+        // 최소 몇판 더 해야 승률이 변하지?
+        // 앞으로의 모든 게임에서 지지 않음
+        int left = 1;
+        int right = 1_000_000_000;
         while (left < right) {
-            long mid = (left + right) / 2;
-            if (100 * (Y + mid) / (X + mid) > Z) {
+            int mid = left + (right - left) / 2;
+            if (winRateChange(mid)) {
                 right = mid;
             } else {
                 left = mid + 1;
             }
         }
+        if (!winRateChange(left)) {
+            return -1;
+        }
+        return left;
+    }
 
-        System.out.println(left);
+    private static boolean winRateChange(int mid) {
+        long Z = y * 100L / x;
+        long newZ = (y + mid) * 100L / (x + mid);
+        return Z < newZ;
+    }
+
+    private static void init() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(input.readLine());
+        x = Integer.parseInt(tokenizer.nextToken());
+        y = Integer.parseInt(tokenizer.nextToken());
     }
 }
