a76cb401f46a55d4c3b58bec7c9b8dc29c9423a0
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2025-12-03 14:05:59 +0900

    [Gold IV] Title: 서강그라운드, Time: 76 ms, Memory: 12160 KB -BaekjoonHub

--- 백준/Gold/14938. 서강그라운드/서강그라운드.java
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -9,7 +9,49 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         init();
-        findAnswer();
+        int answer = findAnswer();
+
+        System.out.println(answer);
+    }
+
+    private static int findAnswer() {
+        int maxItem = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            int item = findItem(i);
+            maxItem = Math.max(maxItem, item);
+        }
+
+        return maxItem;
+    }
+
+    private static int findItem(int start) {
+        int sum = 0;
+        int[] dist = new int[n  +1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        Deque<int[]> que = new ArrayDeque<>();
+        que.offer(new int[]{start, 0});
+        dist[start] = 0;
+
+        while (!que.isEmpty()) {
+            int[] cur = que.poll();
+            int curNode = cur[0];
+            int curDist = cur[1];
+
+            for (int[] next : graph.get(curNode)) {
+                if (dist[next[0]] > curDist + next[1]) {
+                    dist[next[0]] = curDist + next[1];
+                    que.offer(new int[]{next[0], curDist + next[1]});
+                }
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (dist[i] <= m) {
+                sum += items[i];
+            }
+        }
+
+        return sum;
     }
 
     private static void init() throws IOException {
@@ -18,62 +60,24 @@ public class Main {
         n = Integer.parseInt(tokenizer.nextToken());
         m = Integer.parseInt(tokenizer.nextToken());
         r = Integer.parseInt(tokenizer.nextToken());
-        items = new int[n];
-        graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
-        }
+        items = new int[n + 1];
         tokenizer = new StringTokenizer(input.readLine());
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i <= n; i++) {
             items[i] = Integer.parseInt(tokenizer.nextToken());
         }
+
+        graph = new ArrayList<>();
+        for (int i = 0; i < n + 1; i++) {
+            graph.add(new ArrayList<>());
+        }
+
         for (int i = 0; i < r; i++) {
             tokenizer = new StringTokenizer(input.readLine());
-            int a = Integer.parseInt(tokenizer.nextToken());
-            int b = Integer.parseInt(tokenizer.nextToken());
+            int from = Integer.parseInt(tokenizer.nextToken());
+            int to = Integer.parseInt(tokenizer.nextToken());
             int cost = Integer.parseInt(tokenizer.nextToken());
-            graph.get(a).add(new int[]{b, cost});
-            graph.get(b).add(new int[]{a, cost});
+            graph.get(from).add(new int[]{to, cost});
+            graph.get(to).add(new int[]{from, cost});
         }
-    }
-
-    private static void findAnswer() {
-        int max = Integer.MIN_VALUE;
-        for (int i = 1; i <= n; i++) {
-            int sum = findMaxSum(i);
-            max = Math.max(max, sum);
-        }
-
-        System.out.println(max);
-    }
-
-    private static int findMaxSum(int idx) {
-        int sum = items[idx - 1];
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[idx] = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        pq.add(new int[] {idx, 0});
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int curIdx = cur[0];
-            int cost = cur[1];
-            for (int[] next : graph.get(curIdx)) {
-                if (dist[next[0]] > cost + next[1]) {
-                    dist[next[0]] = cost + next[1];
-                    pq.offer(new int[] {next[0], cost + next[1]});
-                }
-            }
-        }
-        for (int i = 1; i <= n; i++) {
-            if (i == idx) {
-                continue;
-            }
-            if (dist[i] <= m) {
-                sum+=items[i - 1];
-            }
-        }
-
-        return sum;
     }
 }
