4556973c65ce483e074ad6886e9362f6fdac2f0b
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2026-01-03 17:37:18 +0900

    [Gold V] Title: 선 긋기, Time: 700 ms, Memory: 31880 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -3,58 +3,68 @@ import java.util.*;
 
 public class Main {
 
-    private static int n;
-    private static int[][] seg;
+    static class FastScanner {
+        private final InputStream in = System.in;
+        private final byte[] buffer = new byte[1 << 16];
+        private int ptr = 0, len = 0;
 
-    public static void main(String[] args) throws IOException {
-        init();
-        long answer = findAnswer();
-
-        System.out.println(answer);
-    }
-
-    private static void init() throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-
-        n = Integer.parseInt(input.readLine());
-        seg = new int[n][2];
-
-        for (int i = 0; i < n; i++) {
-            StringTokenizer tokenizer = new StringTokenizer(input.readLine());
-            int x = Integer.parseInt(tokenizer.nextToken());
-            int y = Integer.parseInt(tokenizer.nextToken());
-            seg[i][0] = x;
-            seg[i][1] = y;
+        private int readByte() throws IOException {
+            if (ptr >= len) {
+                len = in.read(buffer);
+                ptr = 0;
+                if (len <= 0) return -1;
+            }
+            return buffer[ptr++];
         }
 
-        Arrays.sort(seg, Comparator.comparingInt(a -> a[0]));
+        int nextInt() throws IOException {
+            int c;
+            do { c = readByte(); } while (c <= ' ');
+
+            int sign = 1;
+            if (c == '-') { sign = -1; c = readByte(); }
+
+            int val = 0;
+            while (c > ' ') {
+                val = val * 10 + (c - '0');
+                c = readByte();
+            }
+            return val * sign;
+        }
     }
 
-    private static long findAnswer() {
-        // 선을 그을건데 그 선이 그어지는 총 길이를 구하는거고
-        // 선의 길이는 end - start로 간주하는 모양
-        // 그렇다면 우선 순위 큐로 시작점 정렬을 해놓고 구간이 끝날 떄 길이 sum에 더해주는 방식을 사용하는게 나아보임
-        // 범위는 -1,000,000,000 <= p <= 1,000,000,000
-        // 병합하면서 갱신해나가야 함
-        int curStart = seg[0][0];
-        int curEnd = seg[0][1];
+    public static void main(String[] args) throws Exception {
+        FastScanner fs = new FastScanner();
+        int n = fs.nextInt();
+
+        long[] seg = new long[n];
+        for (int i = 0; i < n; i++) {
+            int x = fs.nextInt();
+            int y = fs.nextInt();
+            // x 오름차순 정렬되게 상위 32비트에 x, 하위에 y
+            seg[i] = (((long) x) << 32) ^ (y & 0xffffffffL);
+        }
+
+        Arrays.sort(seg);
+
+        long curStart = (int) (seg[0] >> 32);
+        long curEnd   = (int) seg[0];
         long sum = 0;
 
-        for (int i = 1; i < seg.length; i++) {
-            int start = seg[i][0];
-            int end = seg[i][1];
+        for (int i = 1; i < n; i++) {
+            long start = (int) (seg[i] >> 32);
+            long end   = (int) seg[i];
 
-            if (curEnd >= start) {
-                curEnd = Math.max(curEnd, end);
+            if (start <= curEnd) {
+                if (end > curEnd) curEnd = end;
             } else {
-                sum += curEnd - curStart;
+                sum += (curEnd - curStart);
                 curStart = start;
                 curEnd = end;
             }
         }
-        // 마지막 보정이 필요한거 같은데
         sum += (curEnd - curStart);
 
-        return sum;
+        System.out.println(sum);
     }
 }
