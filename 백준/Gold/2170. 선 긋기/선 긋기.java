68eb5e3bbcf8c1874836eae94c78ab7bd2532fb4
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2026-01-03 17:35:48 +0900

    [Gold V] Title: 선 긋기, Time: 2924 ms, Memory: 283240 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.java
+++ 백준/Gold/2170. 선 긋기/선 긋기.java
@@ -4,55 +4,50 @@ import java.util.*;
 public class Main {
 
     private static int n;
-    private static PriorityQueue<int[]> pq;
+    private static int[][] seg;
 
     public static void main(String[] args) throws IOException {
         init();
-        int answer = findAnswer();
+        long answer = findAnswer();
 
         System.out.println(answer);
     }
 
     private static void init() throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
 
         n = Integer.parseInt(input.readLine());
+        seg = new int[n][2];
 
         for (int i = 0; i < n; i++) {
             StringTokenizer tokenizer = new StringTokenizer(input.readLine());
             int x = Integer.parseInt(tokenizer.nextToken());
             int y = Integer.parseInt(tokenizer.nextToken());
-            pq.offer(new int[]{x, y});
+            seg[i][0] = x;
+            seg[i][1] = y;
         }
+
+        Arrays.sort(seg, Comparator.comparingInt(a -> a[0]));
     }
 
-    private static int findAnswer() {
+    private static long findAnswer() {
         // 선을 그을건데 그 선이 그어지는 총 길이를 구하는거고
         // 선의 길이는 end - start로 간주하는 모양
         // 그렇다면 우선 순위 큐로 시작점 정렬을 해놓고 구간이 끝날 떄 길이 sum에 더해주는 방식을 사용하는게 나아보임
         // 범위는 -1,000,000,000 <= p <= 1,000,000,000
         // 병합하면서 갱신해나가야 함
-        int curStart = -1_000_000_001;
-        int curEnd = -1_000_000_001;
-        int sum = 0;
+        int curStart = seg[0][0];
+        int curEnd = seg[0][1];
+        long sum = 0;
 
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int start =  cur[0];
-            int end = cur[1];
-
-            if (curStart == curEnd) {
-                curStart = start;
-                curEnd = end;
-                continue;
-            }
+        for (int i = 1; i < seg.length; i++) {
+            int start = seg[i][0];
+            int end = seg[i][1];
 
             if (curEnd >= start) {
                 curEnd = Math.max(curEnd, end);
             } else {
-                // 기존 범위를 벗어남
-                sum += (curEnd - curStart);
+                sum += curEnd - curStart;
                 curStart = start;
                 curEnd = end;
             }
