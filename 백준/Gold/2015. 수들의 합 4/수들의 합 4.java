042fe1acc20f7d7207950aa6fd44538acb78696b
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2026-01-31 13:55:02 +0900

    [Gold IV] Title: 수들의 합 4, Time: 296 ms, Memory: 33484 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.java
@@ -1,44 +1,46 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
-    private static int n;
-    private static int k;
-    private static long[] arr;
-    private static long[] prefixSum;
+    private static int n, k;
+    private static int[] num;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        init(input);
+        init();
+        long answer = findAnswer();
+
+        System.out.println(answer);
     }
 
-    private static void init(BufferedReader input) throws IOException {
+    private static void init() throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer tokenizer = new StringTokenizer(input.readLine());
         n = Integer.parseInt(tokenizer.nextToken());
         k = Integer.parseInt(tokenizer.nextToken());
-        arr = new long[n];
-        prefixSum = new long[n + 1];
 
+        num = new int[n];
         tokenizer = new StringTokenizer(input.readLine());
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Long.parseLong(tokenizer.nextToken());
+        for (int i = 0; i < n; i++) {
+            num[i] = Integer.parseInt(tokenizer.nextToken());
+        }
+    }
+
+    private static long findAnswer() {
+        Map<Long, Long> freq = new HashMap<>();
+        long sum = 0;
+        long count = 0;
+        freq.put(0L, 1L);
+
+        for (int i : num) {
+            sum += i;
+
+            long need = sum - k;
+            count += freq.getOrDefault(need, 0L);
+
+            freq.put(sum, freq.getOrDefault(sum, 0L) + 1);
         }
 
-        long cnt = 0;
-        Map<Long, Long> map = new HashMap<>();
-        map.put(0L, 1L);
-        for (int i = 1; i < prefixSum.length; i++) {
-            prefixSum[i] = prefixSum[i - 1] + arr[i - 1];
-        }
-
-        for (int i = 1; i < prefixSum.length; i++) {
-            cnt += map.getOrDefault(prefixSum[i] - k, 0L);
-            map.put(prefixSum[i], map.getOrDefault(prefixSum[i], 0L) + 1);
-        }
-
-        System.out.println(cnt);
+        return count;
     }
 }
