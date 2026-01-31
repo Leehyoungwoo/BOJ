f38012d52bbeb35eaa33c2868ba65999e56db5eb
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2026-01-31 15:15:47 +0900

    [Gold IV] Title: 점수따먹기, Time: 460 ms, Memory: 19084 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.java
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 public class Main {
 
-    private static int n;
-    private static int m;
+    private static int n, m;
     private static int[][] map;
-    private static int[][] prefix;
 
     public static void main(String[] args) throws IOException {
         init();
-        findAnswer();
+        int answer = findAnswer();
+
+        System.out.println(answer);
     }
 
     private static void init() throws IOException {
@@ -18,41 +18,39 @@ public class Main {
         StringTokenizer tokenizer = new StringTokenizer(input.readLine());
         n = Integer.parseInt(tokenizer.nextToken());
         m = Integer.parseInt(tokenizer.nextToken());
-        map = new int[n + 1][m + 1];
-        prefix = new int[n + 1][m + 1];
+        map = new int[n][m];
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             tokenizer = new StringTokenizer(input.readLine());
-            for (int j = 1; j <= m; j++) {
+            for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(tokenizer.nextToken());
-            }
-        }
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                prefix[i][j] = map[i][j] + prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1];
             }
         }
     }
 
-    private static void findAnswer() {
-        int maxSum = Integer.MIN_VALUE;
+    private static int findAnswer() {
+        int max = Integer.MIN_VALUE;
+        // 누적합을 구해서 완전탐색 (n, m <= 200)
+        int[][] prefix = new int[n + 1][m + 1];
 
-        for (int r1 = 1; r1 <= n; r1++) {
-            for (int r2 = r1; r2 <= n; r2++) {
-                for (int c1 = 1; c1 <= m; c1++) {
-                    for (int c2 = c1; c2 <= m; c2++) {
-                        int sum = prefix[r2][c2]
-                                - (r1 > 1 ? prefix[r1 - 1][c2] : 0)
-                                - (c1 > 1 ? prefix[r2][c1 - 1] : 0)
-                                + (r1 > 1 && c1 > 1 ? prefix[r1 - 1][c1 - 1] : 0);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + map[i - 1][j - 1];
+            }
+        }
 
-                        maxSum = Math.max(maxSum, sum);
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                for (int k = 0; k < i; k++) {
+                    for (int l = 0; l < j; l++) {
+                        int target = prefix[i][j] - prefix[k][j] - prefix[i][l] + prefix[k][l];
+
+                        max = Math.max(target, max);
                     }
                 }
             }
         }
 
-        System.out.println(maxSum);
+        return max;
     }
 }
