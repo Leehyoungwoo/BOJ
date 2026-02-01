7a766171538907f3830ec0ad936e74d926a1c1b5
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2026-02-01 21:21:23 +0900

    [Silver IV] Title: 수 찾기, Time: 140 ms, Memory: 3024 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+vector<int> num;
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        num.push_back(x);
+    }
+
+    sort(num.begin(), num.end());
+    cin >> m;
+    string result;
+    for (int i = 0; i < m; i++) {
+        int target;
+        cin >> target;
+        
+        int count = upper_bound(num.begin(), num.end(), target) - lower_bound(num.begin(), num.end(), target);
+        if (count == 0) {
+            result+="0\n";
+        } else {
+            result+="1\n";
+        }
+    }
+
+    cout << result;
+}
