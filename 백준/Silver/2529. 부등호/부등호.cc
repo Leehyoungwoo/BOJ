837fe377dc20c2b4aed6d70675b0f72d395cc5a0
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2026-02-08 21:03:43 +0900

    [Silver I] Title: 부등호, Time: 56 ms, Memory: 3576 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.cc
+++ 백준/Silver/2529. 부등호/부등호.cc
@@ -6,11 +6,13 @@ int b[10];
 vector<string> ret;
 bool valid() {
     for (int i = 0; i < k; i++) {
+        // i번째가 i+1보다 작아야 하는데 크면 false;
         if (a[i] == '<') {
             if (b[i] > b[i + 1]) {
                 return false;
             }
         } else {
+            // i번쨰가 i+1보다 커야 하는데 작으면 false;
             if (b[i] < b[i + 1]) {
                 return false;
             }
@@ -20,15 +22,16 @@ bool valid() {
 }
 
 void make_per(int n, int r, int depth) {
+    // nPr 순열을 구하고 
     if (r == depth) {
-        // for (int i = 0; i < r; i++) cout << b[i] << " ";
-        // cout << "\n";
+        // 유효성 검사를 통과하면 
         if (valid()) {
             string s;
+            // 붙여서 
             for (int i = 0; i < k + 1; i++){
                 s+=to_string(b[i]);
             }
-            // cout << s << "\n";
+            // ret에 넣어주고
             ret.push_back(s);
         }
         return;
@@ -48,6 +51,7 @@ int main() {
         b[i] = i;
     }
     make_per(10, k + 1, 0);
+    // 정렬해서 제일 큰거, 작은거 출력
     sort(ret.begin(), ret.end());
     cout << ret[(int)(ret.size()) - 1] << "\n";
     cout << ret[0] << "\n";
