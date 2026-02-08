ad663b49d6297a8209fb225f59d2ab8538cd05fd
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2026-02-08 14:27:25 +0900

    [Gold V] Title: 보물섬, Time: 120 ms, Memory: 2168 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -38,6 +38,8 @@ int bfs(int r, int c) {
     return dis;
 }
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
         string s;
