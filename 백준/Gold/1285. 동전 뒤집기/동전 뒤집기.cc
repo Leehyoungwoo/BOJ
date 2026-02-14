c6093d2822c6c47658e120645a6df03811559be1
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2026-02-14 13:13:53 +0900

    [Gold I] Title: 동전 뒤집기, Time: 236 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
@@ -8,6 +8,7 @@
     int min_cnt = INT_MAX;
     vector<pair<int, int>> v;
     // 앞면 H, 뒷면 T
+    // 행 뒤집음을 바탕으로 열을 탐색, 열에 뒷면이 더 많으면 뒤집었다고 가정해서 더 적은 것을 누적 뒷면을 더 적게 만들어야하니까
     int calculate() {
         int count = 0;
         for (int i = 0; i < n; i++) {
@@ -35,8 +36,10 @@
         }
         
         go(start + 1);
-        turn_row(start);
+        coin[start] = ~coin[start];
         go(start + 1);
+        // 원복
+        coin[start] = ~coin[start];
     }
     int main(){
         cin >> n;
