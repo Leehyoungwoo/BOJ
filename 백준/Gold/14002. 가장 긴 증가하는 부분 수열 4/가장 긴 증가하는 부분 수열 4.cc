a6379212fb8eabec9f4779bd14aab7e784b83836
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2026-02-28 15:29:29 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,43 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
-int idx;
-int ret = 1;
 int cnt[1001];
-int prev_list[1001];
 int a[1001];
+int prev_list[1001];
 vector<int> v;
+int ret = 1;
+int idx;
 void go(int idx) {
-    if (idx == -1) {
-        return;
-    }
+    if (idx == -1) return;
     v.push_back(a[idx]);
     go(prev_list[idx]);
-    return; 
 }
 int main() {
     cin >> n;
-    for (int i = 0; i < n; i++) cin >> a[i];
-    fill(prev_list, prev_list + 1001, -1);
+    for(int i = 0; i < n; i++) cin >> a[i];
     fill(cnt, cnt + 1001, 1);
+    fill(prev_list, prev_list + 1001, -1);
+
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i; j++) {
-            if(a[j] < a[i] && cnt[i] < cnt[j] + 1){
+            if (a[j] < a[i] && cnt[i] < cnt[j] + 1) {
                 cnt[i] = cnt[j] + 1;
                 prev_list[i] = j;
-                if (ret < cnt[i]) {
-                    ret = cnt[i];
-                    idx = i;
-                }
             }
+        }
+        if (ret < cnt[i]) {
+            ret = cnt[i];
+            idx = i;
         }
     }
     cout << ret << '\n';
     go(idx);
-    string ar;
-    for (int i = v.size() - 1; i >= 0; i--) {
-        ar += to_string(v[i]) + " ";
-    }
-    cout << ar << '\n';
+    for (int i = v.size() - 1; i >= 0; i--) cout << v[i] << ' ';
     return 0;
 }
