0f5710bcea6dea5502c9570dd4d079259e4bbbbd
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2026-03-02 14:08:19 +0900

    [Gold V] Title: 전깃줄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.cc
+++ 백준/Gold/2565. 전깃줄/전깃줄.cc
@@ -4,24 +4,24 @@ int n;
 int a[501];
 int cnt[501];
 int ret;
-int main(){
+int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
-        int idx, h;
-        cin >> idx >> h;
-        a[idx] = h;
+        int idx, end;
+        cin >> idx >> end;
+        a[idx] = end;
     }
-    // 교차되지 않는다 -> end 위치 기준으로 lis면 교차가 안됨 -> 전체 -lis인듯?
-    for (int i = 0; i < 502; i++) {
-        int maxvalue = 0;
+
+    for(int i = 0; i < 501; i++) {
+        int maxValue = 0;
         if (a[i] == 0) continue;
         for (int j = 0; j < i; j++) {
             if (a[j] == 0) continue;
-            if (a[j] < a[i] && maxvalue < cnt[j]) {
-                maxvalue = cnt[j];
+            if (a[j] < a[i] && maxValue < cnt[j]) {
+                maxValue = cnt[j];
             }
         }
-        cnt[i] = maxvalue + 1;
+        cnt[i] = maxValue + 1;
         ret = max(ret, cnt[i]);
     }
 
