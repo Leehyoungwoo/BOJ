3aec15455b24ec8e7b456f225015f9561b11ba2b
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2026-03-02 21:23:23 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -1,23 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
-int cnt[1001];
 int a[1001];
+int cnt[1001];
 int prev_list[1001];
-vector<int> v;
-int ret = 1;
+int ret;
 int idx;
+vector<int> v;
 void go(int idx) {
     if (idx == -1) return;
     v.push_back(a[idx]);
     go(prev_list[idx]);
-}
-int main() {
+} 
+int main(){
     cin >> n;
-    for(int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> a[i];
     fill(cnt, cnt + 1001, 1);
     fill(prev_list, prev_list + 1001, -1);
-
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i; j++) {
             if (a[j] < a[i] && cnt[i] < cnt[j] + 1) {
@@ -30,6 +29,7 @@ int main() {
             idx = i;
         }
     }
+
     cout << ret << '\n';
     go(idx);
     for (int i = v.size() - 1; i >= 0; i--) cout << v[i] << ' ';
