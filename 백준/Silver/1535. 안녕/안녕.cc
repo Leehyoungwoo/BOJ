32f3f4e3c29a56304af7baaecc8db6f60ea63134
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2026-03-10 20:02:03 +0900

    [Silver II] Title: 안녕, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/1535. 안녕/안녕.cc
+++ 백준/Silver/1535. 안녕/안녕.cc
@@ -1,19 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
-int hp[21];
-int hppy[21];
-int dp[101];
-int main() {
-    cin >> n;
-    for (int i = 0; i < n; i++) cin >> hp[i];
-    for (int i = 0; i < n; i++) cin >> hppy[i];
+int l[21];
+int j[21];
+int dp[21][101];
+int hp = 100;
+int go(int idx, int hp) {
+    if (hp <= 0) return -1e9;
+    if (idx == n) return 0;
     
-    for(int i = 0; i < n; i++) {
-        for (int h = 99; h >= hp[i]; h--) {
-            dp[h] = max(dp[h], dp[h - hp[i]] + hppy[i]);
-        }
-    }
-    cout << dp[99] << '\n';
+    int &ret = dp[idx][hp];
+    if (ret != -1) return ret;
+    ret = go(idx + 1, hp);
+    if (hp - l[idx] > 0) ret = max(ret , go(idx + 1, hp - l[idx]) + j[idx]);
+
+    return ret;
+}
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++) cin >> l[i];
+
+    for (int i = 0; i < n; i++) cin >> j[i];
+
+    memset(dp, -1, sizeof(dp));
+    cout << go(0, 100) << '\n';
     return 0;
 }
