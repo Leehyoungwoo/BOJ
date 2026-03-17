e3e8ef21f14a58c0f7b43ea9dbc74bc4188a5c8a
Leehyoungwoo <112301121+Leehyoungwoo@users.noreply.github.com>
2026-03-17 19:44:53 +0900

    [Gold II] Title: 게임, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.cc
+++ 백준/Gold/1103. 게임/게임.cc
@@ -1,47 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m;
-string s;
+string input;
+const int dir[4][2] = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 char mp[51][51];
 int dp[51][51];
-int ret;
-const int dr[4] = {1, -1, 0, 0};
-const int dc[4] = {0, 0, -1, 1};
 bool visited[51][51];
+bool flag;
 bool in(int r, int c) {
-    return 0 <= r && r < n && 0 <= c && c < m && isdigit(mp[r][c]);
+    return 0 <= r && r < n && 0 <= c && c < m;
 }
 int go(int r, int c) {
-    int &ret = dp[r][c];
-    if (ret != -1) return ret;
-    ret = 1;
-    int cur = mp[r][c] - '0';
-    for (int i = 0; i < 4; i++) {
-        int nr = r + dr[i] *  cur;
-        int nc = c + dc[i] * cur;
-
-        if (!in(nr, nc)) continue;
-        if (visited[nr][nc]) return 1e9;
-
+    int & ret = dp[r][c];
+    if (ret != -1) return dp[r][c];
+    ret = 0;
+    for (auto d : dir) {
+        int nr = r + d[0] * (mp[r][c] - '0');
+        int nc = c + d[1] * (mp[r][c] - '0');
+        if (!in(nr, nc) || mp[nr][nc] == 'H') continue;
+        if (visited[nr][nc]) {
+            flag = true;
+            return 0;
+        }
         visited[nr][nc] = true;
         ret = max(ret, go(nr, nc) + 1);
         visited[nr][nc] = false;
     }
 
-    return ret;
+    return ret; 
 }
 int main() {
     cin >> n >> m;
     for (int i = 0; i < n; i++) {
-        cin >> s;
+        cin >> input;
         for (int j = 0; j < m; j++) {
-            mp[i][j] = s[j];
+            mp[i][j] = input[j];
         }
     }
+
     memset(dp, -1, sizeof(dp));
     visited[0][0] = true;
-    int ret =  go(0, 0);
-    if (ret >= 1e9) cout << -1 << '\n';
-    else cout << ret << '\n';
+    int ret = go(0, 0);
+    if (flag) cout << -1 << '\n';
+    else cout << ret + 1 << '\n';
     return 0;
 }
